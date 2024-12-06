a863492ae588ef9f320df46687eea94515380ec7
Manuel Peters <manuel.peters@stud.tu-darmstadt.de>
2024-12-06 15:18:11 +0100

    initial student repo

--- src/main/java/h10/Main.java
+++ src/main/java/h10/Main.java
@@ -4,12 +4,17 @@ package h10;
  * Main entry point in executing the program.
  */
 public class Main {
+
     /**
      * Main entry point in executing the program.
      *
      * @param args program arguments, currently ignored
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Manual testing
+        CardGame game = CardGame.generateRandomCardGame();
+        System.out.println(game);
+        CardGamePlayer loser = game.determineLoser();
+        System.out.println("The loser is: " + loser.getName());
     }
 }
